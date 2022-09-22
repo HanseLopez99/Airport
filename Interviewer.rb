a9b82381432f5e125736a7aafd9cac1862caabec
Hansel López <halm29799@gmail.com>
2022-09-22 09:35:30 -0600

    FIx interviewer dialog

--- Interviewer.rb
+++ Interviewer.rb
@@ -4,23 +4,23 @@ class Interviewer < Employee
   prepend Work
 
   def introduce 
-    puts "I'm an interviewer. I'm going to ask some questions to the people in the airport and show the environment here."
+    puts "Interviewer: I'm an interviewer. I'm going to ask some questions to the people in the airport and show the environment here."
   end
 
   def interview
-    puts "Please tell me about yourself and what are you doing here."
+    puts "Interviewer: Please tell me about yourself and what are you doing here."
   end
 
   def say_thanks
-    puts "Thank you for your time."
+    puts "Interviewer: Thank you for your time."
   end
 
   def say_lets_find_anothe_one
-    puts "Let's find another one."
+    puts "Interviewer: Let's find another one."
   end
 
   def listen_to_animals_area
-    puts "Ohh look the animals, there is a lot of sound."
+    puts "Interviewer: Ohh look at the animals, there is a lot of sound."
   end
 end
 
